d1b240988ee927260ee4d997b7bf26384fbd8fb5
Gustavo <gustavo.moura.sti3@gmail.com>
2021-10-20 23:12:48 -0300

    Criação da classe Conta e do Enum do tipo da Conta, e metodos da conta para saque, deposito, transferencia

---
+++ DIO.Bank/Enum/TipoConta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Bank.Enum
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
